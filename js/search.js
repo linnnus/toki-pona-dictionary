e5657370f7acd30ee5019973b4c8806265689f09
pleasedontlookitsawfull <linusvejlo@gmail.com>
2020-08-10 15:55:58 +0200

    "optimize" js

--- js/search.js
+++ js/search.js
@@ -1,68 +1,36 @@
 function search(term, includeTypes){
-  // console.log('searching for "' + term + '"');
+	// console.log('searching for "' + term + '"');
 
-  let res = [];
+	let res = [];
 
-  dict.forEach(entry => {
+	dict.forEach(entry => {
 
-    let match = false;
-    let reg = new RegExp(String.raw`(?<=\s|^|\p{P})${term}(?=\s|\p{P}|$)`, 'iu');
+		// let match = false;
+		let reg = new RegExp(String.raw`(?<=\s|^|\p{P})${term}(?=\s|\p{P}|$)`, 'iu');
 
+		/*
+			really want to do a switch but appareantly im incepable of doing so
+		*/
 
-    // switch?
+		if ( entry.word && reg.test(entry.word) ) {
+			res.push(entry);
+		} else if (	entry.alternatives
+			&& entry.alternatives.some(str => match = reg.test(str)) ) {
+			res.push(entry);
+		} else if ( entry.alternatives
+			&& entry.alternatives.some(str => match = reg.test(str)) ) {
+			res.push(entry);
+		} else if ( entry.definitions 
+			&& entry.definitions.some(str => match = reg.test(str)) ){
+			res.push(entry);
+		} else if ( entry.translations
+			&& Object.values(entry.translations).some(str => match = reg.test(str)) ){
+			res.push(entry);
+		}
 
-    // check alternatives
-    if(entry.alternatives){
+	});
 
-      entry.alternatives.forEach(str => match = reg.test(str) ? true : match );
-
-      if (match) {
-        // console.log(entry.word, 'matched in alternatives');
-        res.push(entry);
-        return; // no need to search further
-      }
-
-    }
-
-    //check translations
-    if(entry.translations){
-
-      for ( let key in entry.translations){
-        match = reg.test(entry.translations[key]) ? true : match;
-      }
-
-      if (match) {
-        // console.log(entry.word, 'matched in translations');
-        res.push(entry);
-        return; // no need to search further
-      }
-
-    }
-
-    //check definitions (they differ slightly)
-    // will also allow us to search for type
-    if(entry.definitions){
-
-      entry.definitions.forEach( def => {
-
-        // match = def.type.match(reg) ? true : match;
-        // match = def.definition.match(reg) ? true : match;
-        match = reg.test(def.type) ? true : match;
-        match = reg.test(def.definition) ? true : match;
-
-      });
-
-      if (match) {
-        // console.log(entry.word, 'matched in definitions');
-        res.push(entry);
-        return; // no need to search further
-      }
-
-    }
-
-  });
-
-  // console.log('search returned:', res);
-  populateList(res);
+	// console.log('search returned:', res);
+	populateList(res);
 
 }
