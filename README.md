e844052b9ed9539c4545942b0d43b1643687a4c8
pleasedontlookitsawfull <linusvejlo@gmail.com>
2020-11-29 03:39:32 +0100

    remake the entire thing

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 quick dictionary i made for me to use while learning toki pona
 
 # TODO
-* use rem in css?
-* clean up css
-* polyfill var() stuff and generally improve cross-browser support
+* fix css
+	* use rem in css?
+	* clean up css
+	* polyfill var() stuff and generally improve cross-browser support
+	* spacing is generally a bit off
 * fix that dictionary. its wack
 	* definitions are kinda meg
 	* some examples are waaaaay too long
+	* theres a lot of useless information
 	* some definitions are missing examples  
 * add tamplate definitions, to comply with material design
+* fix the footer; that should clearly be a list
+* add multiple dictionaries.
---
+++ js/app.mjs
@@ -0,0 +1,66 @@
+// add elements
+import dictionary from '../data/dictionary.mjs';
+const list = document.getElementById('entries');
+const addElt = (entry) => {
+	const entryElt = document.createElement('SPAN');
+	entryElt.classList.add('entry');
+
+		// word
+		const wordElt = document.createElement('H2');
+		wordElt.textContent = entry.word;
+		wordElt.classList.add('entry-word');
+		entryElt.insertAdjacentElement('beforeend', wordElt);
+
+		// meanings
+		for (const meaning of entry.meanings) {
+			const meaningElt = document.createElement('P');
+			meaningElt.classList.add('entry-meaning');
+
+				// type
+				const typeElt = document.createElement('SPAN');
+				typeElt.classList.add('entry-meaning-type');
+				typeElt.textContent = meaning.type;
+				meaningElt.insertAdjacentElement('beforeend', typeElt);
+
+				// meaning
+				const textElt = document.createElement('SPAN');
+				textElt.classList.add('entry-meaning-text');
+				textElt.textContent = meaning.text;
+				meaningElt.insertAdjacentElement('beforeend', textElt);
+
+			entryElt.insertAdjacentElement('beforeend', meaningElt);
+		};
+
+	return list.insertAdjacentElement('beforeend', entryElt);
+};
+const entries = dictionary.map((entry) => {
+	return {
+		...entry,
+		elt: addElt(entry),
+	}
+});
+
+const searchForm = document.getElementById('form');
+const searchText = document.getElementById('text');
+searchForm.onsubmit = (e) => {
+	e.preventDefault();
+	document.activeElement.blur(); // (hopfully) hide softkeyboard
+
+	const reg = new RegExp(String.raw`\b${searchText.value}\b`, 'iu');
+	for (const entry of entries) {
+		if (!entry.word.match(reg)) {
+			entry.elt.classList.add('hide');
+		} else {
+			entry.elt.classList.remove('hide');
+		}
+	}
+};
+const book = document.getElementById('book');
+book.onclick = (e) => {
+	for (const entry of entries) {
+		entry.elt.classList.remove('hide');
+	}
+};
+
+const loader = document.getElementById('loader');
+loader.classList.add('hide');
