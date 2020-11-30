e8243a40e67b217a89ed0ad27b717a5e905ef1da
pleasedontlookitsawfull <linusvejlo@gmail.com>
2020-11-30 20:57:43 +0100

    restructure js

--- js/app.mjs
+++ js/app.mjs
@@ -1,42 +1,10 @@
 // add elements
 import dictionary from '../data/dictionary.mjs';
-const list = document.getElementById('entries');
-const addElt = (entry) => {
-	const entryElt = document.createElement('SPAN');
-	entryElt.classList.add('entry');
-
-		// word
-		const wordElt = document.createElement('H2');
-		wordElt.textContent = entry.word;
-		wordElt.classList.add('entry-word');
-		entryElt.insertAdjacentElement('beforeend', wordElt);
-
-		// meanings
-		for (const meaning of entry.meanings) {
-			const meaningElt = document.createElement('P');
-			meaningElt.classList.add('entry-meaning');
-
-				// type
-				const typeElt = document.createElement('SPAN');
-				typeElt.classList.add('entry-meaning-type');
-				typeElt.textContent = meaning.type;
-				meaningElt.insertAdjacentElement('beforeend', typeElt);
-
-				// meaning
-				const textElt = document.createElement('SPAN');
-				textElt.classList.add('entry-meaning-text');
-				textElt.textContent = meaning.text;
-				meaningElt.insertAdjacentElement('beforeend', textElt);
-
-			entryElt.insertAdjacentElement('beforeend', meaningElt);
-		};
-
-	return list.insertAdjacentElement('beforeend', entryElt);
-};
+import { createElement } from './utils.mjs';
 const entries = dictionary.map((entry) => {
 	return {
 		...entry,
-		elt: addElt(entry),
+		elt: createElement(entry),
 	}
 });
 
--- js/utils.mjs
+++ js/utils.mjs
@@ -1,3 +1,37 @@
+const list = document.getElementById('entries');
+export const createElement = (entry) => {
+	const entryElt = document.createElement('SPAN');
+	entryElt.classList.add('entry');
+
+		// word
+		const wordElt = document.createElement('H2');
+		wordElt.textContent = entry.word;
+		wordElt.classList.add('entry-word');
+		entryElt.insertAdjacentElement('beforeend', wordElt);
+
+		// meanings
+		for (const meaning of entry.meanings) {
+			const meaningElt = document.createElement('P');
+			meaningElt.classList.add('entry-meaning');
+
+				// type
+				const typeElt = document.createElement('SPAN');
+				typeElt.classList.add('entry-meaning-type');
+				typeElt.textContent = meaning.type;
+				meaningElt.insertAdjacentElement('beforeend', typeElt);
+
+				// meaning
+				const textElt = document.createElement('SPAN');
+				textElt.classList.add('entry-meaning-text');
+				textElt.textContent = meaning.text;
+				meaningElt.insertAdjacentElement('beforeend', textElt);
+
+			entryElt.insertAdjacentElement('beforeend', meaningElt);
+		};
+
+	return list.insertAdjacentElement('beforeend', entryElt);
+};
+
 export const escapeReg = (string) => {
 	return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 };
